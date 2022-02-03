5dee4ed79da083eadfa63dda259804c6fcc03f3e
Carlos Baraza Haro <carlos.baraza@ieee.org>
2022-02-03 13:41:40 +0000

    serve statically builds

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 # Run build setting relative path for build folder
-lerna exec -- \
-    PUBLIC_URL="\$(echo \$PWD | sed \"s|\$LERNA_ROOT_PATH||g\")/build" \
-        npm run build
+# lerna exec -- \
+#     PUBLIC_URL="\$(echo \$PWD | sed \"s|\$LERNA_ROOT_PATH||g\")/build" \
+#         npm run build
+
+# Add build folder
+rm -rf build
+mkdir -p build
+
+# Challenge 1: React
+mkdir -p build/challenges/component-challenge-1/component-challenge-1-react/build
+cp -r \
+    challenges/component-challenge-1/component-challenge-1-react/build/ \
+    build/challenges/component-challenge-1/component-challenge-1-react/build/
+
+cp -r src/ build/
