812383195da348a241f45c5fdbba8ef1e5079e9f
carlosbaraza <carlosbaraza@users.noreply.github.com>
2022-02-03 16:06:34 +0000

    deploy: bed9aca29521f00e1a10c668bbea78edb9dd9b56

---
+++ _next/static/lRT-au467rrPSTB9bSPv5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6667f82e360be92c.css","static/chunks/pages/index-e56ae08686e692d6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
