824b1a3dab4782900f17c32786609c597e6cf696
Carlos Baraza Haro <carlos.baraza@ieee.org>
2022-02-03 12:38:26 +0000

    lerna dependencies

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install dependencies
-lerna bootstrap
-
 # Run build setting relative path for build folder
 lerna exec -- \
     PUBLIC_URL="\$(echo \$PWD | sed \"s|\$LERNA_ROOT_PATH||g\")/build" \
