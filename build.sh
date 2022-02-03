bed9aca29521f00e1a10c668bbea78edb9dd9b56
Carlos Baraza Haro <carlos.baraza@ieee.org>
2022-02-03 16:03:47 +0000

    Add main docs page

--- build.sh
+++ build.sh
@@ -2,17 +2,20 @@
 
 # Run build setting relative path for build folder
 lerna exec -- \
-    PUBLIC_URL="\$(echo \$PWD | sed \"s|\$LERNA_ROOT_PATH|/component-challenges/src|g\")/build" \
+    PUBLIC_URL="\$(echo \$PWD | sed \"s|\$LERNA_ROOT_PATH|/component-challenges|g\")" \
         npm run build
 
 # Add build folder
 rm -rf build
 mkdir -p build
 
+# Docs page
+cp -r \
+    docs/out/* \
+    build
+
 # Challenge 1: React
 mkdir -p build/challenges/component-challenge-1/component-challenge-1-react/build
 cp -r \
     challenges/component-challenge-1/component-challenge-1-react/build/* \
     build/challenges/component-challenge-1/component-challenge-1-react
-
-cp -r src/* build
